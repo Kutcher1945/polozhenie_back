21611b473048ba90bb0207f18d2f266300c18f30
Adilan Akhramovich <a.akhramovich@almatydc.kz>
2024-10-27 18:57:52 +0500

    added a new functions

--- address/models/district.py
+++ address/models/district.py
@@ -6,11 +6,11 @@ from .district_akim import CityDistrictAkim
 
 class CityDistrict(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name_kz = models.CharField(max_length=255, verbose_name='Название на казахском')
-    name_ru = models.CharField(max_length=255, verbose_name='Название на русском')
-    response_name_kz = models.CharField(max_length=255, verbose_name='Произношение на казахском')
-    response_name_ru = models.CharField(max_length=255, verbose_name='Произношение на русском')
-    gerb_img = models.CharField(max_length=255, verbose_name='Герб')
+    name_kz = models.CharField(max_length=255, verbose_name='Название на казахском', blank=True, null=True)
+    name_ru = models.CharField(max_length=255, verbose_name='Название на русском', blank=True, null=True)
+    response_name_kz = models.CharField(max_length=255, verbose_name='Произношение на казахском', blank=True, null=True)
+    response_name_ru = models.CharField(max_length=255, verbose_name='Произношение на русском', blank=True, null=True)
+    gerb_img = models.CharField(max_length=255, verbose_name='Герб', blank=True, null=True)
     akim = models.ForeignKey(CityDistrictAkim, models.DO_NOTHING, verbose_name="Аким", blank=True, null=True)
     geometry = models.MultiPolygonField(verbose_name="Геометрия", blank=True, null=True)
     marker = models.PointField(verbose_name="Маркер", blank=True, null=True)
