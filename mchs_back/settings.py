0d2abdd2dfbd57c3b055ec07b7513d69186d74df
Adilan Akhramovich <a.akhramovich@almatydc.kz>
2024-11-04 12:40:00 +0500

    added a new cors allowed origins

--- mchs_back/settings.py
+++ mchs_back/settings.py
@@ -98,9 +98,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # React app
-    'http://localhost:8000',  # Django API
+    'http://localhost:3000',  # React app Local
+    'http://localhost:8000',  # Django API Local
+    'https://exp.smartalmaty.kz/',  # React app Prod
+    'https://exp-admin.smartalmaty.kz/',  # Django API Prod
 ]
+
+
 
 TEMPLATES = [
     {
