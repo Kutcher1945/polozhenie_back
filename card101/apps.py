8b2570554045a8e5c3085bb9f67c53ae1beebf60
Adilan Akhramovich <a.akhramovich@almatydc.kz>
2025-04-27 15:25:53 +0500

    added a new info

--- card101/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class Card101Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'card101'
-    verbose_name = 'Карточка 101'
