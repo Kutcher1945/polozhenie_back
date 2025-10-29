9f97c5a3be4343669e47817d0cafb07377587802
Akhramovich Adilan <adilan.akhramovich@gmail.com>
2025-10-29 21:58:53 +0500

    fixed admin login

--- common/models.py
+++ common/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth.base_user import BaseUserManager
 
 
 class BaseModel(models.Model):
@@ -24,6 +25,34 @@ class DoctorSpecialization(BaseModel):
         db_table = "common_doctors_specialization"
         verbose_name = "Специализация врача"
         verbose_name_plural = "Специализации врачей"
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email address must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_password(self.make_random_password())
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(BaseModel):
@@ -141,6 +170,9 @@ class User(BaseModel):
     # Django authentication requirements
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+    EMAIL_FIELD = 'email'
+
+    objects = UserManager()
 
     def set_password(self, raw_password):
         """Hashes and saves the password"""
