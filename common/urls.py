c67fd8a5a20109aa24dd5f4556ff0c91374937b7
Adilan Akhramovich <adilan.akhramovich@gmail.com>
2026-01-31 19:27:19 +0500

    added anew auth system

--- common/urls.py
+++ common/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     UserViewSet, UserProfileViewSet, StaffViewSet,
-    ClinicsViewSet, PatientsViewSet, SessionViewSet, csrf_cookie_view
+    ClinicsViewSet, PatientsViewSet, SessionViewSet, ReportsViewSet, csrf_cookie_view
 )
 
 router = DefaultRouter()
@@ -13,6 +13,7 @@ router.register(r'staff', StaffViewSet, basename='staff')
 router.register(r'clinics', ClinicsViewSet, basename='clinics')
 router.register(r'patients', PatientsViewSet, basename='patients')
 router.register(r'sessions', SessionViewSet, basename='sessions')
+router.register(r'reports', ReportsViewSet, basename='reports')
 
 urlpatterns = [
     path('auth/csrf/', csrf_cookie_view, name='csrf-cookie'),
